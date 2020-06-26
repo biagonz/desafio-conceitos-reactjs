f1c59302cba24a3a796b6e5896b9df4d110e9241
Beatriz Gonçalves <biagonz@live.com>
2020-06-25 23:53:45 -0300

    Funções de listar, adicionar e remover itens

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `Projeto ${Date.now()}`,
+      url: "https://github.com/biagonz",
+      techs: ["Node", "JS"]
+    });
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const reponse = api.delete(`repositories/${id}`);
+    const index = repositories.findIndex(repo => repo.id === id);
+    const listRepos = [...repositories];
+    listRepos.splice(index, 1);
+    setRepositories(listRepos);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repo => (
+          <li key={repo.id}>
+            {repo.title}
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+              </button>
+          </li>
+        ))
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
